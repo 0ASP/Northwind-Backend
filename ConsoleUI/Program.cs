debe738b5ad44481bf420739e9d01c7e6c48d425
Felaxi <Felaxi@DESKTOP-P3I1KES>
2022-07-26 16:14:20 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrate;
+using DataAccess.Concrate.EntityFramework;
+using DataAccess.Concrate.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName); 
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrate/EntityFramework/NortwindContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrate.EntityFramework
+{
+    public class NortwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-P3I1KES;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Costumer> Costurmers { get; set; }
+    }
+}
